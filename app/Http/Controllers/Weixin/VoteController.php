4941222e18d1ffb50c29cc003a831dd960380f7f
mxl <1113943731@qq.com>
2019-12-18 14:39:01 +0800

    获取token

--- app/Http/Controllers/Weixin/VoteController.php
+++ app/Http/Controllers/Weixin/VoteController.php
@@ -11,7 +11,21 @@ class VoteController extends Controller
 
     public  function index(){
 
-        dd($_GET);
+       $code=$_GET['code'];
+        $state=$_GET['state'];
+        //获取code后，请求以下链接获取access_token
+        $data=$this->access_token($code);
+        dd($data);
+
+    }
+
+    protected function access_token($code){
+
+        $url='ttps://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECREET').'&code='.$code.'&grant_type=authorization_code';
+        $data=file_get_contents($url);
+
+        return json_decode($data,true);
+
     }
 
 
