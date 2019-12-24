0e00c45c828f9ca485592f639d5f6ee83a0194f5
mxl <1113943731@qq.com>
2019-12-24 10:28:19 +0800

    分享

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Model\GoodsModel;
 use App\Model\WxUserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class IndexController extends Controller
 {
