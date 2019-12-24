1d8417aa2641a5f2f176dde36e68a1c72c0db194
mxl <1113943731@qq.com>
2019-12-24 10:32:39 +0800

    分享

--- app/Model/WxUserModel.php
+++ app/Model/WxUserModel.php
@@ -11,6 +11,20 @@ class WxUserModel extends Model
     protected  $primaryKey='uid';
 
 
+    protected function getAccessToken()
+    {
+        $key = 'wx_access_token';
+        $access_token = Redis::get($key);
+        if($access_token){
+            return $access_token;
+        }
+        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_APPSECREET');
+        $data_json = file_get_contents($url);
+        $arr = json_decode($data_json,true);
+        Redis::set($key,$arr['access_token']);
+        Redis::expire($key,3600);
+        return $arr['access_token'];
+    }
     /**
      * 获取jsapi_ticket
      * @return mixed
