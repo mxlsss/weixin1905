00a06901650479b2a3572eff394c0ac53a33ef62
mxl <1113943731@qq.com>
2019-12-26 12:15:39 +0800

    群发

--- app/Http/Controllers/Kaoshi/KaoshiController.php
+++ app/Http/Controllers/Kaoshi/KaoshiController.php
@@ -235,7 +235,7 @@ class KaoshiController extends Controller
         $client = new Client();
 
         $aaa=$client->request('POST',$url,[
-            'body'=>json_encode($data,JSON_UNESCAPED_UNICODE),
+            'body'=>json_encode($data,JSON_UNESCAPED_UNICODE)
         ]);
         echo $aaa->getBody();
     }
